7c92ff64c5803b31fdf82dc6be785f15fe62cbfb
Felipe Borges <felipetecacc@gmail.com>
2024-08-30 17:36:29 -0300

    first commit

---
+++ aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -0,0 +1,30 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Amanda Barros", "amanda@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Roberta Souza", "roberta@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
+	}
+
+}
